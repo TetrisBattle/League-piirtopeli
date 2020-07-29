fd151931d6995280878d74f2d7bc3d5dc3fcf748
Eetu Jussila <eetu.juss@live.fi>
2020-07-30 00:06:06 +0300

    Buttonit toimii

--- canvas.js
+++ canvas.js
@@ -1,13 +1,28 @@
 window.addEventListener("load", () => {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
+ 
 
     //resizing
-    canvas.height = "1000";
-    canvas.width = "1400";
+    canvas.height = window.innerHeight/100*99;
+    canvas.width = window.innerWidth/100*99;
+
 
     //variables 
     let painting = false;
+    let bttn1 = document.querySelector("#button1");
+    let bttn2 = document.querySelector("#button2");
+    let bttn3 = document.querySelector("#button3");
+    let bttn4 = document.querySelector("#button4");
+    let bttn5 = document.querySelector("#button5");
+    let bttn6 = document.querySelector("#button6");
+    let bttn7 = document.querySelector("#button7");
+    let bttn8 = document.querySelector("#button8");
+    let bttn9 = document.querySelector("#button9");
+    let bttn10 = document.querySelector("#button10");
+    let bttn11 = document.querySelector("#button11");
+    let bttn12 = document.querySelector("#button12");
+    let x = 10;
 
     function startPosition(e){
         painting = true;
@@ -19,22 +34,31 @@ window.addEventListener("load", () => {
     }
     function draw(e){
         if(!painting) return;
-        //ctx.lineWidth = 5;
-        ctx.lineWidth = 10;
-        //ctx.lineWidth = 15;
-        //ctx.lineCap = "triangle";
-        //ctx.lineCap = "square";
+        ctx.lineWidth = x;
         ctx.lineCap = "round";
-        ctx.strokeStyle = "Black";
 
-        ctx.lineTo(e.clientX-530, e.clientY-95);
+        ctx.lineTo(e.clientX, e.clientY);
         ctx.stroke();
         ctx.beginPath();
-        ctx.moveTo(e.clientX-530, e.clientY-95);
+        ctx.moveTo(e.clientX, e.clientY);
     }
     //Eventlisteners
+    bttn1.addEventListener('click', () => ctx.strokeStyle ="black")
+    bttn2.addEventListener('click', () => ctx.strokeStyle ="red")
+    bttn3.addEventListener('click', () => ctx.strokeStyle ="green")
+    bttn4.addEventListener('click', () => ctx.strokeStyle ="blue")
+    bttn5.addEventListener('click', () => ctx.strokeStyle ="purple")
+    bttn6.addEventListener('click', () => ctx.strokeStyle ="yellow")
+    bttn7.addEventListener('click', () => ctx.strokeStyle ="orange")
+    bttn8.addEventListener('click', () => ctx.strokeStyle ="pink")
+    bttn9.addEventListener('click', () => ctx.strokeStyle ="white")
+    bttn9.addEventListener('click', () => x = 30)
+    bttn10.addEventListener('click', () => x = 5)
+    bttn11.addEventListener('click', () => x = 10)
+    bttn12.addEventListener('click', () => x = 20)
     canvas.addEventListener('mousedown', startPosition);
     canvas.addEventListener('mouseup', finishedPosition);
     canvas.addEventListener('mousemove', draw);
+
 
 });
