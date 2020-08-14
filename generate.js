21b2977d3ec6d665c8ac1b45f44a587da3888284
Thien <thien5596@gmail.com>
2020-08-14 15:21:07 +0300

    champ pics

--- generate.js
+++ generate.js
@@ -1,3 +1,6 @@
+var picLink = [];
+var x = 0;
+
 function generate() {
     var Champion_name = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bard", "Blitzcrank",
         "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", 
@@ -9,8 +12,16 @@ function generate() {
         "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", 
         "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", 
         "Xerath", "Xin Zhao", "Yone", "Yasuo", "Yorick", "Yuumi", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"];
-    var rand_Champion_name = Math.floor(Math.random()*Champion_name.length);
-    var rand_Champion_name2 = Math.floor(Math.random()*Champion_name.length);
-    var rand_Champion_name3 = Math.floor(Math.random()*Champion_name.length);
-    document.getElementById('results').innerHTML = "<h2>3 Champion Names in buttons:</h2><div><button id=Champ_btn1 class=btn2>"+Champion_name[rand_Champion_name]+"</button></div><div><button id=Champ_btn2 class=btn2>"+Champion_name[rand_Champion_name2]+"</button></div><div><button id=Champ_btn3 class=btn2>"+Champion_name[rand_Champion_name3]+"</button></div>"; 
+        
+    var rand_Champion_name = [Math.floor(Math.random()*Champion_name.length), Math.floor(Math.random()*Champion_name.length), Math.floor(Math.random()*Champion_name.length)];
+
+    for(var i=0; i<3; i++){
+        var results = "results" + i.toString();
+        document.getElementById(results).innerHTML = Champion_name[rand_Champion_name[i]];
+        picLink[i] = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + Champion_name[rand_Champion_name[i]] + "_0.jpg";
+    }
+}
+
+function newPic(x){
+    document.getElementById('skin').src = picLink[x];
 }
